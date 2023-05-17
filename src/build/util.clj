dd950f64555c2d3076a3ddcf9a810683585992ca
Miloslav Nenadal <nenadalm@gmail.com>
2023-05-17 19:38:18 +0200

    Change cache busting of js (filename instead of query string)

--- src/build/util.clj
+++ src/build/util.clj
@@ -1,6 +1,20 @@
 (ns build.util
   (:require
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.edn]))
+
+(def ^:private module-id->output-name (atom nil))
+
+(defn- output-name [module-id]
+  (when module-id
+    (when-not @module-id->output-name
+      (reset!
+       module-id->output-name
+       (into {}
+             (map (juxt :module-id :output-name))
+             (clojure.edn/read-string (slurp (io/resource "public/js/manifest.edn"))))))
+    (when-let [s (@module-id->output-name module-id)]
+      (str "js/" s))))
 
 (defn- sha-1 [s]
   (let [c (java.security.MessageDigest/getInstance "sha-1")]
@@ -17,5 +31,13 @@
     (file-hash url)
     (throw (ex-info "Asset not found." {:path path}))))
 
+(defn- module-id [path]
+  (-> (re-matches #"^js/(.+)\.js" path)
+      second
+      keyword))
+
 (defn asset [path]
-  (str path "?v=" (asset-hash path)))
+  (or
+   (-> (module-id path)
+       output-name)
+   (str path "?v=" (asset-hash path))))
