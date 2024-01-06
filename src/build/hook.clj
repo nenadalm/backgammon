7622de62937d282a1a3efb963013a939c9e5b87b
Miloslav Nenadal <nenadalm@gmail.com>
2024-01-06 10:15:30 +0100

    Css cache busting improvement (file name instead of query)

--- src/build/hook.clj
+++ src/build/hook.clj
@@ -3,11 +3,14 @@
    [clojure.java.io :as io]
    [build.create-manifest]
    [build.create-index]
-   [build.create-worker]))
+   [build.create-worker]
+   [build.util :as u]))
 
-(defn- module-id->output-name [build-state]
-  (-> (group-by :module-name (:shadow.build.closure/modules build-state))
-      (update-vals #(-> % first :output-name))))
+(defn- file->output-name [build-state]
+  (-> (group-by (fn [m]
+                  (str "js/" (m :module-name))) (:shadow.build.closure/modules build-state))
+      (update-vals (fn [coll]
+                     (str "js/" (-> coll first :output-name))))))
 
 (defn- path [s]
   (java.nio.file.Paths/get s (make-array java.lang.String 0)))
@@ -27,18 +30,23 @@
      target-f)))
 
 (defn- create-styles [build-mode]
-  (let [src "resources/private/css/styles.css"
-        target "resources/public/css/styles.css"]
+  (let [src "resources/private/css/styles.css"]
     (case build-mode
-      :release (copy src target)
-      (symlink target src))))
+      :release
+      (let [hash (u/file-hash src)
+            target (str "resources/public/css/styles." hash ".css")]
+        (copy src target)
+        {"css/styles.css" (str "css/styles." hash ".css")})
+      (do
+        (symlink "resources/public/css/styles.css" src)
+        {}))))
 
 (defn hook
   {:shadow.build/stage :flush}
   [build-state & _args]
   (when-not (.exists (io/file "resources/public/worker.js"))
-    (let [outputs (module-id->output-name build-state)]
-      (create-styles (:shadow.build/mode build-state))
+    (let [outputs (merge (file->output-name build-state)
+                         (create-styles (:shadow.build/mode build-state)))]
       (spit "resources/public/manifest.json" (build.create-manifest/render outputs))
       (spit "resources/public/index.html" (build.create-index/render outputs))
       (spit "resources/public/worker.js" (build.create-worker/render outputs))))
--- src/build/util.clj
+++ src/build/util.clj
@@ -3,17 +3,15 @@
    [clojure.java.io :as io]
    [clojure.edn]))
 
-(defn- output-name [module-name module-id->output-name]
-  (when module-name
-    (when-let [s (module-id->output-name module-name)]
-      (str "js/" s))))
+(defn- output-name [module-name file->output-name]
+  (file->output-name module-name))
 
 (defn- sha-1 [s]
   (let [c (java.security.MessageDigest/getInstance "sha-1")]
     (.update c (.getBytes s "utf-8"))
     (reduce (fn [s b] (str s (format "%02x" b))) "" (.digest c))))
 
-(defn- file-hash [f]
+(defn file-hash [f]
   (-> f
       slurp
       sha-1))
@@ -23,12 +21,7 @@
     (file-hash url)
     (throw (ex-info "Asset not found." {:path path}))))
 
-(defn- module-name [path]
-  (-> (re-matches #"^js/(.+\.js)" path)
-      second))
-
 (defn asset [path module-id->output-name]
   (or
-   (-> (module-name path)
-       (output-name module-id->output-name))
+   (output-name path module-id->output-name)
    (str path "?v=" (asset-hash path))))
